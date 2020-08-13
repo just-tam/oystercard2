334ef81dd51a1974a139c48d05753b745c1ab0db
Tamara Jeffrey <just-tam@live.com>
2020-08-13 20:21:24 +0100

    failing test for deducting money from balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,6 +23,13 @@ describe Oystercard do
       subject.top_up(maximum_balance)
       expect{ subject.top_up 1 }.to raise_error "The maximum balance is £#{maximum_balance}"
     end
+  end
 
+  describe '#deduct' do
+    it "Expects money to be deducted from card" do
+      maximum_balance = Oystercard::MAX_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.deduct 1 }.to change{ subject.balance }.by 1
+    end
   end
 end
