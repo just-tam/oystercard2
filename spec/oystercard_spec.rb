324f91f5c295b0abad528a2e7f6b11ddd36c863e
Tamara Jeffrey <just-tam@live.com>
2020-08-13 19:28:40 +0100

    failing test for top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,8 @@ describe Oystercard do
   it "Checks balance is equal to zero" do
     expect(subject.balance).to eq (0)
   end
+
+  it "Expects oystercard to respond to method top_up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
 end
